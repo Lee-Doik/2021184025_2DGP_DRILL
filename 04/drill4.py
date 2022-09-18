391f76a99ea7789f4d8f679e9dde37beb8727e40
Lee-Doik <idoik2002@gmail.com>
2022-09-18 17:45:31 +0900

    Drill#4 최종
    2021184025 이도익

--- 04/drill4.py
+++ 04/drill4.py
@@ -1,92 +1,29 @@
 import turtle
 
 turtle.speed(0)
-count = 5
-x = 0
+count1 = 5
+count2 = 5
+y = 100
 
-while (count > 0):
-    turtle.goto(x,0)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    count = count -1
-    x = x + 100
+while (count2 > 0):
+    count1 = 5
+    x = 0
+    while (count1 > 0):
+        turtle.goto(x,y)
+        turtle.pendown()
+        turtle.forward(100)
+        turtle.right(90)
+        turtle.forward(100)
+        turtle.right(90)
+        turtle.forward(100)
+        turtle.right(90)
+        turtle.forward(100)
+        turtle.right(90)
+        count1 = count1 -1
+        x = x + 100
+    y = y + 100
+    count2 = count2 - 1
+    turtle.penup()
 
-turtle.penup()
-x = 0
-count = 5
-
-while (count > 0):
-    turtle.goto(x,100)
-    turtle.pendown()
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    count = count -1
-    x = x + 100
-
-turtle.penup()
-x = 0
-count = 5
-
-while (count > 0):
-    turtle.goto(x,200)
-    turtle.pendown()
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    count = count -1
-    x = x + 100
-
-turtle.penup()
-x = 0
-count = 5
-
-while (count > 0):
-    turtle.goto(x,300)
-    turtle.pendown()
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    count = count -1
-    x = x + 100
-
-turtle.penup()
-x = 0
-count = 5
-
-while (count > 0):
-    turtle.goto(x,400)
-    turtle.pendown()
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    count = count -1
-    x = x + 100
 
 turtle.exitonclick()
