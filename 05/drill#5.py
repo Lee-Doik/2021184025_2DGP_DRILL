42062dec35f18d0a2cd824e3e86c17b7f6681b2d
Lee-Doik <idoik2002@gmail.com>
2022-09-19 20:47:06 +0900

    Drill#5
    2021184025 이도익

---
+++ 05/drill#5.py
@@ -0,0 +1,37 @@
+import turtle
+
+turtle.speed(0)
+turtle.shape('turtle')
+
+def up_turtle():
+	turtle.setheading(90)
+	turtle.forward(50)
+	turtle.stamp()
+
+def down_turtle():
+	turtle.setheading(270)
+	turtle.forward(50)
+	turtle.stamp()
+
+def left_turtle():
+	turtle.setheading(180)
+	turtle.forward(50)
+	turtle.stamp()
+	
+def right_turtle():
+	turtle.setheading(0)
+	turtle.forward(50)
+	turtle.stamp()
+
+def restart():
+	turtle.reset()
+	turtle.speed(0)
+
+turtle.onkey(up_turtle, 'w')
+turtle.onkey(down_turtle, 's')
+turtle.onkey(left_turtle, 'a')
+turtle.onkey(right_turtle, 'd')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
+
+turtle.exitonclick()
