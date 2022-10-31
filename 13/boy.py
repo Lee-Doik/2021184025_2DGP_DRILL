0f4c186fee1c88c56f31f84cce4de02e514102f6
Lee-Doik <idoik2002@gmail.com>
2022-10-31 15:16:01 +0900

    DRILL#11
    2021184025 이도익

--- 13/boy.py
+++ 13/boy.py
@@ -1,115 +1,176 @@
 from pico2d import *
 
-# 이베느 정의
-#RD, LD, RU, LU = 0, 1, 2, 3
-RD, LD, RU, LU = range(4)
+RD, LD, RU, LU, AD, TIMER = range(6)
 
 key_event_table = {
-    (SDL_KEYDOWN, SDLK_RIGHT) : RD,
-    (SDL_KEYDOWN, SDLK_LEFT) : LD,
-    (SDL_KEYUP, SDLK_RIGHT) : RU,
-    (SDL_KEYUP, SDLK_LEFT) : LU,
+    (SDL_KEYDOWN, SDLK_RIGHT)   : RD,
+    (SDL_KEYDOWN, SDLK_LEFT)    : LD,
+    (SDL_KEYUP, SDLK_RIGHT)     : RU,
+    (SDL_KEYUP, SDLK_LEFT)      : LU,
+    (SDL_KEYDOWN, SDLK_a)       : AD,
 }
 
-
-
-# 스테이트를 구현 - 클래스를 이용해서
-class IDLE:
+class SLEEP:
     @staticmethod
-    def enter():
-        print('ENTER IDLE')
+    def enter(self, event):
+        print('ENTER SLEEP')
+        self.dir = 0
+        if event == RD:
+            self.dir += 1
+        elif event == LD:
+            self.dir -= 1
+        elif event == RU:
+            self.dir -= 1
+        elif event == LU:
+            self.dir += 1
+
+    @staticmethod
+    def exit(self):
+        print('EXIT SLEEP')
         pass
 
     @staticmethod
-    def exit():
+    def do(self):
+        self.frame = (self.frame + 1) % 8
+
+    @staticmethod
+    def draw(self):
+        if self.face_dir == 1:
+            self.image.clip_composite_draw(self.frame * 100, 300, 100, 100, -3.141592/2, '', self.x, self.y-25, 100, 100)
+        else:
+            self.image.clip_composite_draw(self.frame * 100, 200, 100, 100, 3.141592/2, '', self.x, self.y-25, 100, 100)
+
+
+class IDLE:
+    @staticmethod
+    def enter(self,event):
+        print('ENTER IDLE')
+        self.timer = 1000
+        if event == RD:
+            self.dir += 1
+        elif event == LD:
+            self.dir -= 1
+        elif event == RU:
+            self.dir -= 1
+        elif event == LU:
+            self.dir += 1
+
+    @staticmethod
+    def exit(self):
         print('EXIT IDLE')
         pass
     @staticmethod
-    def do():
-        pass
+    def do(self):
+        self.frame = (self.frame + 1)  % 8
+        self.timer -= 1
+        if self.timer == 0:
+            self.add_event(TIMER)
     @staticmethod
-    def draw():
-        pass
+    def draw(self):
+        if self.face_dir == 1:
+            self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
+
 
 class RUN:
 
-    def enter():
+    def enter(self, event):
         print('ENTER RUN')
-        pass
+        if event == RD:
+            self.dir += 1
+        elif event == LD:
+            self.dir -= 1
+        elif event == RU:
+            self.dir -= 1
+        elif event == LU:
+            self.dir += 1
 
-    def exit():
+    def exit(self):
         print('EXIT RUN')
-        pass
+        self.face_dir = self.dir
 
-    def do():
-        pass
+    def do(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += self.dir
+        self.x = clamp(0, self.x, 800)
 
-    def draw():
-        pass
+    def draw(self):
+        #print('DRAW RUN')
+        if self.dir == -1:
+            self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+        elif self.dir == 1:
+            self.image.clip_draw(self.frame*100, 100, 100, 100, self.x, self.y)
 
+class AUTO_RUN:
+    def enter(self, event):
+        print('ENTER AUTO_RUN')
+        self.auto_dir = self.face_dir
+        if event == RD:
+            self.auto_dir += 1
+        elif event == LD:
+            self.auto_dir -= 1
+        elif event == RU:
+            self.auto_dir -= 1
+        elif event == LU:
+            self.auto_dir += 1
+
+    def exit(self):
+        print('EXIT AUTO_RUN')
+        self.face_dir = self.auto_dir
+
+    def do(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += self.auto_dir
+        if self.x >= 800:
+            self.auto_dir = -1
+        elif self.x == 0:
+            self.auto_dir = 1
+
+    def draw(self):
+        #print('DRAW AUTO_RUN')
+        if self.auto_dir == -1:
+            self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y+50, 200, 200)
+        elif self.auto_dir == 1:
+            self.image.clip_draw(self.frame*100, 100, 100, 100, self.x, self.y+50, 200, 200)
+
+
+# 상태 변환
 next_state = {
-    IDLE : {RU:RUN, LU:RUN, RD:RUN, LD:RUN},
-    RUN : { RU:IDLE, LU:IDLE, LD:IDLE, RD:IDLE}
+    SLEEP: {RU: RUN, LU: RUN, RD: RUN, LD: RUN, AD: AUTO_RUN, TIMER: SLEEP},
+    IDLE: {RU: RUN, LU: RUN, RD: RUN, LD: RUN, AD: AUTO_RUN, TIMER: SLEEP},
+    RUN: {RU: IDLE, LU: IDLE, LD: IDLE, RD: IDLE, AD: AUTO_RUN, TIMER: RUN},
+    AUTO_RUN: {RU: RUN, LU: RUN, LD: RUN, RD: RUN, AD: IDLE},
 }
-
 
 class Boy:
 
-    def add_event(self, key_event):
-        self.q.insert(0, key_event)
+    def __init__(self):
+        self.x, self.y = 800 // 2, 90
+        self.frame = 0
+        self.dir, self.face_dir, self.auto_dir = 0, 1, 1
+        self.image = load_image('animation_sheet.png')
 
-    def handle_event(self, event):#키입력 이벤트
+        self.event_que = []
+        self.cur_state = IDLE
+        self.cur_state.enter(self, None)
+
+    def update(self):
+        self.cur_state.do(self)
+
+    def draw(self):
+        self.cur_state.draw(self)
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+    def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
             key_event = key_event_table[(event.type, event.key)]
             self.add_event(key_event)
-        if self.q:# 만약에 list q에 뭔가 들어있으면
-            event = self.q.pop()
-            self.cur_state.exit()
-            self.cur_state =  next_state[self.cur_state][event]
-            self.cur_state.enter()
 
-        # if event.type == SDL_KEYDOWN:
-        #     match event.key:
-        #         case pico2d.SDLK_LEFT:
-        #             self.dir -= 1
-        #         case pico2d.SDLK_RIGHT:
-        #             self.dir += 1
-        # elif event.type == SDL_KEYUP:
-        #     match event.key:
-        #         case pico2d.SDLK_LEFT:
-        #             self.dir += 1
-        #             self.face_dir = -1
-        #         case pico2d.SDLK_RIGHT:
-        #             self.dir -= 1
-        #             self.face_dir = 1
-
-    def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
-        self.dir, self.face_dir = 0, 1
-        self.image = load_image('animation_sheet.png')
-
-        self.q = []
-        # 초기 상태 설정과
-        self.cur_state = IDLE
-        self.cur_state.enter()
-
-    def update(self):
-        self.cur_state.do()
-
-        #self.frame = (self.frame + 1) % 8
-        #self.x += self.dir * 1
-        #self.x = clamp(0, self.x, 800)
-
-    def draw(self):
-        self.cur_state.draw()
-
-#        if self.dir == -1:
-#            self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
-#        elif self.dir == 1:
-#            self.image.clip_draw(self.frame*100, 100, 100, 100, self.x, self.y)
-#        else:
-#            if self.face_dir == 1:
-#                self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
-#            else:
-#                self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
+        if self.event_que:
+            event = self.event_que.pop()
+            self.cur_state.exit(self)
+            self.cur_state = next_state[self.cur_state][event]
+            self.cur_state.enter(self, event)
